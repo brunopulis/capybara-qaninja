9c746e90c576900bc0a0c1927e3de3764e0a9f94
Bruno Pulis <bruno.pulis@monetizze.com.br>
2020-05-10 20:57:49 -0300

    feat: automatiza drag n drop

---
+++ spec/drag_drop_spec.rb
@@ -0,0 +1,17 @@
+describe 'Drag n Drop', :drop do  
+  before(:each) do
+    visit 'https://training-wheels-protocol.herokuapp.com/drag_and_drop'
+  end
+
+  it 'Homem aranha pertence ao time Stark' do
+    stark = find('.team-stark .column')
+    cap   = find('.team-cap .column')
+    spiderman = find('img[alt$=Aranha')
+
+    spiderman.drag_to stark
+
+    expect(stark).to have_css "img[alt$=Aranha]"
+    expect(cap).not_to have_css "img[alt$=Aranha]"
+    sleep 5
+  end
+end
