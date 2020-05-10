4a4893def015c596ad9bcc171b6649c431b87004
Bruno Pulis <bruno.pulis@monetizze.com.br>
2020-05-10 19:28:56 -0300

    feat: automatiza radiobutton

---
+++ spec/radio_spec.rb
@@ -0,0 +1,21 @@
+describe 'Radiobutton', :radio do  
+  before(:each) do
+    visit 'https://training-wheels-protocol.herokuapp.com/radios'
+  end
+
+  it 'Marca um item especifico por ID' do
+    choose('cap')
+  end
+
+  it 'Marca um item por find com css selector' do
+    find('input[value=guardians]').click
+  end
+
+  
+
+  
+
+  after(:each) do
+    sleep 3
+  end
+end
