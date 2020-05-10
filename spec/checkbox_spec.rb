d1a1ccc9aecd144eac0889f21b4fd9740764c65f
Bruno Pulis <bruno.pulis@monetizze.com.br>
2020-05-10 19:17:42 -0300

    feat: adiciona automação para checkbox

---
+++ spec/checkbox_spec.rb
@@ -0,0 +1,25 @@
+describe 'Checkboxes', :checkbox do  
+  before(:each) do
+    visit 'https://training-wheels-protocol.herokuapp.com/checkboxes'
+  end
+
+  it 'Marca um item especifico' do
+    check('thor')
+  end
+
+  it 'Desmarca um item especifico' do
+    uncheck('antman')
+  end
+
+  it 'Marcando com find set true' do
+    find('input[value=cap]').set true
+  end
+
+  it 'Desmarcando com find set false' do
+    find('input[value=guardians]').set false
+  end
+
+  after(:each) do
+    sleep 3
+  end
+end
