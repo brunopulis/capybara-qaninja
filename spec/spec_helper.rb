e6300c87d47a518161c6b1df0b807b9a252d54cf
Bruno Pulis <bruno.pulis@monetizze.com.br>
2020-05-10 17:50:31 -0300

    feat: adiciona configs do Capybara para integrar com o selenium

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require 'capybara'
+require 'capybara/rspec'
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -8,4 +11,10 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.include Capybara::DSL
+end
+
+Capybara.configure do |config|
+  config.default_driver = :selenium_chrome
 end
